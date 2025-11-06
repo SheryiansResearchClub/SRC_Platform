a6d18471cde94b7aed243e3087283c81b534b6b0
Mausam100 <smausam104@gmail.com>
2025-11-06 14:14:35 +0530

    update footer

--- client/src/features/home/components/Mentors.jsx
+++ client/src/features/home/components/Mentors.jsx
@@ -2,37 +2,37 @@ import React from 'react'
 
 const Mentors = () => {
   return (
-    <section id="mentors" class="mentors-scroll">
-      <div class="mentors-wrapper">
-        <div class="mentors-wrapper-flex">
-          <div class="top">
-            <div class="heading">Mentors of the club.</div>
-            <div class="information">we’ve curated the best mentors of the industry to bring the best experience out
+    <section id="mentors" className="mentors-scroll">
+      <div className="mentors-wrapper">
+        <div className="mentors-wrapper-flex">
+          <div className="top">
+            <div className="heading">Mentors of the club.</div>
+            <div className="information">we’ve curated the best mentors of the industry to bring the best experience out
               there.
             </div>
-            <div class="meet">
+            <div className="meet">
               <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6.87793 1.81445V12.5312M6.87793 12.5312L12.2363 7.17285M6.87793 12.5312L1.51953 7.17285"
                   stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
-              <span class="information">meet them</span>
+              <span className="information">meet them</span>
             </div>
 
           </div>
-          <div class="mentors"></div>
-          <div class="full-view">
-            <div class="wrapper">
-              <div class="back">
+          <div className="mentors"></div>
+          <div className="full-view">
+            <div className="wrapper">
+              <div className="back">
                 <svg width="21" height="14" viewBox="0 0 21 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M19.3998 6.99467H1.43945M6.99528 12.5491L1.43945 6.99332L6.99528 1.4375" stroke="white"
                     stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
                 <span>Go Back</span>
               </div>
-              <div class="title">
+              <div className="title">
                 Some Real Heading ®
               </div>
-              <div class="info">
+              <div className="info">
                 faaltu ki baatein and it’s features, tbh we are also searching for what to write, and we have no idea to
                 write
                 anything here.
